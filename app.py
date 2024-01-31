24d992c27dec2aa7db52fb54284083a916ab9acd
bisdo2000 <150210584+bisdo2000@users.noreply.github.com>
2024-01-31 04:14:27 +0000

    Added bootstrap, navbar, inheritance

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/About_me")
+if __name__ == "__main__":
+      app.run(debug=True)
+
+@app.route("/")
 def home():
     return render_template("index.html", title="Hello")
 
